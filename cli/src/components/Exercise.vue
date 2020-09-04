69eab84909b7870146ecf53552124d70725022af
Naung Tun <konaung212245@gmail.com>
2020-09-04 15:02:20 +0630

    vue route

---
+++ cli/src/components/Exercise.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+	<h1 class="text-danger">Exercise Component</h1>
+	<p>This is Exercise for child component</p>
+</div>
+</template>
+
+<script type="text/javascript">
+	export default{
+		
+	}
+</script>
+<style type="text/css">
+	
+</style>
